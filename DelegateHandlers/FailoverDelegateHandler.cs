74dbb8ab4cf907e347eb0d3924f9c46acb1ba10e
Ali Zorlu <azorlua@gmail.com>
2024-06-12 01:29:33 +0300

    Proje dosyası ekle.

---
+++ DelegateHandlers/FailoverDelegateHandler.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Options;
+using Pretech.Ihale.Mobile.Gateway.Settings;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pretech.Ihale.Mobile.Gateway.DelegateHandlers
+{
+    public class FailoverDelegateHandler : DelegatingHandler
+    {
+        private readonly LBSettings _envanters;
+        public FailoverDelegateHandler(IOptions<LBSettings> envanters)
+        {
+            _envanters = envanters?.Value;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                CancellationTokenSource requestCancellation = new CancellationTokenSource(_envanters.HttpRequestSettings.RequestTimeout);
+                try
+                {
+
+                    var response = await base.SendAsync(request, requestCancellation.Token);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return response;
+                    }
+                    else
+                    {
+                        var changeState = RequestChange(request, new UriBuilder(request.RequestUri));
+                        if (!changeState)
+                        {
+                            return response;
+                        }
+                        else continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if ((ex.InnerException is SocketException) | (requestCancellation.IsCancellationRequested))
+                    {
+                        RequestChange(request, new UriBuilder(request.RequestUri));
+                    }
+                }
+            }
+            return await base.SendAsync(request, cancellationToken); ;
+
+        }
+        private bool RequestChange(HttpRequestMessage request, UriBuilder errorHost)
+        {
+            var firstHost = _envanters.Envanters.SkipWhile(q => q.Host == errorHost.Host).FirstOrDefault();
+            if (firstHost == null) return false;
+            else
+            {
+                errorHost.Host = firstHost.Host;
+                errorHost.Port = firstHost.Port;
+                request.RequestUri = errorHost.Uri;
+                return true;
+            }
+        }
+    }
+}
---
+++ Settings/LBSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pretech.Ihale.Mobile.Gateway.Settings
+{
+    public class LBSettings
+    {
+        public Envanters[] Envanters { get; set; }
+        public HttpRequestSettings HttpRequestSettings { get; set; }
+    }
+    public class Envanters
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+    public class HttpRequestSettings
+    {
+        public TimeSpan RequestTimeout { get; set; }
+    }
+}
